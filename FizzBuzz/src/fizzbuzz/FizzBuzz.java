21eddb7e7e424f0d474b459af92231ee1ef20be6
19kimc@smtexas.org <19kimc@smtexas.org>
2016-09-30 14:11:08 -0500

    Chnged

--- FizzBuzz/src/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -7,16 +7,39 @@ package fizzbuzz;
 
 /**
  *
- * @author Mr. T - CHANGE TO YOUR NAME
+ * @author Mr. T - Chad Kim
  * @class AP CSA
  */
 public class FizzBuzz {
 
     /**
-     * @param args the command line arguments
+     * i used a for loop to print the numbers from 1 to 100 and inside that I
+     * inserted an if and if else statement print Fizz when the modulator 
+     * prints 0 and the same concept for printing Buzz in place of multiples of
+     * 5
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /**
+     * i used a for loop to print the numbers from 1 to 100 and inside that I
+     * inserted an if and if else statement print Fizz when the modulator 
+     * prints 0 and the same concept for printing Buzz in place of multiples of
+     * 5
+     */
+        for(int i = 1; i < 101; i++)
+        {
+            if(i % 3 == 0)
+            {   
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }
     }
     
 }
